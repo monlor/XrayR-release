e8054a91a89948d8b6525946c0c26f909ac8c44f
monlor <me@monlor.com>
2023-07-27 10:57:45 +0800

    ✨ feat: 支持reality配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,15 +20,15 @@ ConnectionConfig:
   BufferSize: 64 # The internal cache size of each connection, kB
 Nodes:
   -
-    PanelType: "NewV2board" # Panel type: SSpanel, V2board, NewV2board, PMpanel, Proxypanel, V2RaySocks
+    PanelType: "${PANEL_TYPE:-NewV2board}" # Panel type: SSpanel, V2board, NewV2board, PMpanel, Proxypanel, V2RaySocks
     ApiConfig:
       ApiHost: "${API_HOST}"
       ApiKey: "${API_KEY}"
       NodeID: ${NODE_ID}
-      NodeType: ${NODE_TYPE} # Node type: V2ray, Shadowsocks, Trojan
+      NodeType: ${NODE_TYPE:-V2ray} # Node type: V2ray, Shadowsocks, Trojan
       Timeout: 30 # Timeout for the api request
       EnableVless: ${ENABLE_VLESS:-false} # Enable Vless for V2ray Type
-      EnableXTLS: ${ENABLE_XTLS:-false} # Enable XTLS for V2ray and Trojan
+      VlessFlow: "xtls-rprx-vision" # Only support vless
       SpeedLimit: 0 # Mbps, Local settings will replace remote settings
       DeviceLimit: 0 # Local settings will replace remote settings
       RuleListPath: /etc/XrayR/rulelist # Path to local rulelist file
@@ -37,20 +37,20 @@ Nodes:
       SendIP: 0.0.0.0 # IP address you want to send pacakage
       UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
       EnableDNS: ${ENABLE_DNS:-true} # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: UseIP # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      DNSType: ${DNS_TYPE:-UseIP} # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      EnableProxyProtocol: ${ENABLE_PROXY_PROTOCOL:-false} # Only works for WebSocket and TCP
       # 限速500mbps，5次告警阈值之后限速到100mbps，持续60分钟
       AutoSpeedLimitConfig:
         Limit: 500 # Warned speed. Set to 0 to disable AutoSpeedLimit ()
         WarnTimes: 5 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.
         LimitSpeed: 100 # The speedlimit of a limited user (unit: mbps)
-        LimitDuration: 60 # How many minutes will the limiting last (unit: minute)
+        LimitDuration: 20 # How many minutes will the limiting last (unit: minute)
       GlobalDeviceLimitConfig:
         Enable: ${DEVICE_LIMIT:-false} # Enable the global device limit of a user
         RedisAddr: ${REDIS_ADDR:-127.0.0.1} # The redis server address
         RedisPassword: ${REDIS_PASSWORD:-} # Redis password
         RedisDB: 0 # Redis DB
-        Timeout: 8 # Timeout for redis request
+        Timeout: 5 # Timeout for redis request
         Expiry: 60 # Expiry time (second)
       EnableFallback: ${ENABLE_FALLBACK:-false} # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
@@ -60,6 +60,23 @@ Nodes:
           Path: # HTTP PATH, Empty for any
           Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      EnableREALITY: ${ENABLE_REALITY} # Enable REALITY
+      REALITYConfigs:
+        Show: ${REALITY_DEBUG:-true} # Show REALITY debug
+        Dest: ${REALITY_DEST:-www.smzdm.com:443} # Required, Same as fallback
+        ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable
+        ServerNames: # Required, list of available serverNames for the client, * wildcard is not supported at the moment.
+$(echo ${REALITY_SERVER_NAMES} | tr ',' '\n' | while read domain; do
+  echo "          - ${domain}"
+done)
+        PrivateKey: ${REALITY_PRIVATE_KEY} # Required, execute './xray x25519' to generate.
+        MinClientVer: # Optional, minimum version of Xray client, format is x.y.z.
+        MaxClientVer: # Optional, maximum version of Xray client, format is x.y.z.
+        MaxTimeDiff: 0 # Optional, maximum allowed time difference, unit is in milliseconds.
+        ShortIds: # Required, list of available shortIds for the client, can be used to differentiate between different clients.
+$(echo ${REALITY_SHORT_IDS} | tr ',' '\n' | while read id; do
+  echo "          - ${ids}"
+done)
       CertConfig:
         CertMode: ${CERT_MODE:-http} # Option about how to get certificate: none, file, http, dns
         CertDomain: "${DOMAIN:-}" # Domain to cert
